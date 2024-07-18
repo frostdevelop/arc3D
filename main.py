91e463bb99b50f988859d983e59c5a61fe4986df
Pacifiky <87620151+infinitypacific@users.noreply.github.com>
2024-07-17 23:03:35 -0700

    Update 1.1 main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
+import math
 import pygame as pg
 from pygame.locals import QUIT
 from arcimport import readobj
-from arcdev import *
+from arc import *
 
 '''
 Material Settings:
@@ -55,6 +56,7 @@ def main():
   #scene = Scene([[scube,np.asarray([0,0,0], dtype=np.float32)]], light, [50,127,200])
   #camera = Camera(np.pi/8,[0,0,-10],0,0, width, height)
   camera = Camera(np.pi / 2, (0.0, 1.5, -5.0), (0,0), 1000, 0.5)
+  #camera = Camera(np.pi / 1.1, (0.0, 1.5, -5.0), (0,0), 1000, 0.5)
   renderer = Renderer(width, height, camera, [[1, 0], [0, 1], [1, 1]], False)
   running = True
 
@@ -67,11 +69,6 @@ def main():
       if event.type == pg.KEYDOWN:
         if event.key == pg.K_ESCAPE:
           running = False
-        """
-        if event.key == pg.K_r:
-          croc.mat += 1
-          if croc.mat > 3: croc.mat = 0
-        """
 
     renderer.move(elapsed_time)
     #print(camera.velocity)
@@ -82,13 +79,11 @@ def main():
     scene.objects[1].rotation += elapsed_time/5, 0
     scene.objects[1].upd()
 
-    light.dir = np.array([np.sin(pg.time.get_ticks() / 1000), 1, 1], dtype=np.float32)
+    light.upd((math.sin(pg.time.get_ticks() / 1000), 1, 1))
     #"""
-    #light.dir = light.dir / np.linalg.norm(light.dir)
 
     renderer.render(scene)
     screen.blit(pg.surfarray.make_surface(renderer.surface), (0, 0))
-    #screen.blit(renderer.output, (0, 0))
 
     #"""
     positiontext = font.render(
